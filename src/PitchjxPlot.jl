e0a30c8c594e61b7d2b8054658390bf3caaae63b
miyazaki <scarsdaleny15@yahoo.co.jp>
2019-05-06 14:35:08 +0900

    ~fix include

--- src/PitchjxPlot.jl
+++ src/PitchjxPlot.jl
@@ -11,5 +11,6 @@ using Plots
 include(joinpath(dirname(@__FILE__), "utils.jl"))
 include(joinpath(dirname(@__FILE__), "velo.jl"))
 include(joinpath(dirname(@__FILE__), "heatmap.jl"))
+include(joinpath(dirname(@__FILE__), "movement.jl"))
 
 end # module
